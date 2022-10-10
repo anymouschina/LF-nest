099e971d53188dd5ebce63813122b37e702f68f0
libiqiang <624144061@qq.com>
2022-10-10 22:59:18 +0800

    init: 前端初始化

---
+++ frontend/src/views/editor/editor.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="show-grid">
+
+  </div>
+</template>
+
+<script setup lang="ts">
+
+</script>
+
+<style lang="scss">
+   .show-grid {
+    width: 100%;
+    max-width: 1292px;
+    height: 2400px;
+    position: absolute;
+    background-size: 30.0465px 10px;
+        outline: 1px dotted #4d72da;
+        background-image: linear-gradient(to right,
+            rgba(194, 191, 191, 0.2) 1px,
+            transparent 1px),
+          linear-gradient(to bottom,
+            rgba(194, 191, 191, 0.2) 1px,
+            transparent 1px);
+      }
+</style>
